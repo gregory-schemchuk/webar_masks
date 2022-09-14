c3bfbe2c0602f00461f685f03ba27e2502cf426e
ss <schemchuk.grigor@yandex.ru>
2022-09-14 23:09:42 +0300

    fix v1.8.17

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 // SETTINGS of this demo:
 const SETTINGS = {
-    //gltfModelURL: 'DamagedHelmet/glTF/DamagedHelmet.gltf',
-    gltfModelURL: 'head_test/koltsa.gltf',
+    gltfModelURL: 'DamagedHelmet/glTF/DamagedHelmet.gltf',
+    //gltfModelURL: 'head_test/koltsa.gltf',
     cubeMapURL: 'Bridge2/',
-    //offsetYZ: [0.3, 0], // offset of the model in 3D along vertical and depth axis
-    //scale: 2.5
-    offsetYZ: [-1, -20], // offset of the model in 3D along vertical and depth axis
-    scale: 45
+    offsetYZ: [0.3, 0], // offset of the model in 3D along vertical and depth axis
+    scale: 2.5
+    //offsetYZ: [-1, -20], // offset of the model in 3D along vertical and depth axis
+    //scale: 45
 };
 
 let THREECAMERA = null;
