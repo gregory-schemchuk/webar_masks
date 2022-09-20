2d5c27d3d3db538e9cbbacacd7883966f4329c94
ss <schemchuk.grigor@yandex.ru>
2022-09-20 21:19:18 +0300

    fix v1.8.18.4

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const SETTINGS = {
     gltfModelURL: 'Kokoshnik/Unreal Engine 4.gltf',
     //gltfModelURL: 'head_test/koltsa.gltf',
     cubeMapURL: 'Bridge2/',
-    offsetYZ: [0.3, 0], // offset of the model in 3D along vertical and depth axis
+    //offsetYZ: [0.3, 0], // offset of the model in 3D along vertical and depth axis
+    offsetYZ: [0.3, 3], // offset of the model in 3D along vertical and depth axis
     scale: 2.5
     //offsetYZ: [-1, -20], // offset of the model in 3D along vertical and depth axis
     //scale: 45
