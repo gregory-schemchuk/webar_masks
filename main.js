c0b756a39585c741fdfcd82d7ec0499d0a1588c7
ss <schemchuk.grigor@yandex.ru>
2022-09-21 19:31:14 +0300

    fix v1.8.18.8

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const SETTINGS = {
     //gltfModelURL: 'head_test/koltsa.gltf',
     cubeMapURL: 'Bridge2/',
     //offsetYZ: [0.3, 0], // offset of the model in 3D along vertical and depth axis
-    offsetYZ: [0.3, 0.5], // offset of the model in 3D along vertical and depth axis
+    offsetYZ: [0.3, 0.8], // offset of the model in 3D along vertical and depth axis
     scale: 2.5
     //offsetYZ: [-1, -20], // offset of the model in 3D along vertical and depth axis
     //scale: 45
