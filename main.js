51465542b8e5dc261950e8c29e50af3590f9d62c
ss <schemchuk.grigor@yandex.ru>
2022-09-21 19:37:40 +0300

    fix v1.8.18.8

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const SETTINGS = {
     cubeMapURL: 'Bridge2/',
     //offsetYZ: [0.3, 0], // offset of the model in 3D along vertical and depth axis
     //scale: 2.5
-    offsetYZ: [0.3, 0.8], // offset of the model in 3D along vertical and depth axis
+    offsetYZ: [2, 0], // offset of the model in 3D along vertical and depth axis
     scale: 1.5
     //offsetYZ: [-1, -20], // offset of the model in 3D along vertical and depth axis
     //scale: 45
